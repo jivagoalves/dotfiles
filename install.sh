cc587ef0c4af6dab34519c6a425d27b51db0cc77
Jivago Alves <jivagoalves@gmail.com>
2015-03-17 20:08:39 +0000

    Create ~/.vim/undo dir during installation

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+UNDO_DIR=${HOME}/.vim/undo
+
+if [ ! -d "${UNDO_DIR}" ]; then
+  echo "Creating directory ${UNDO_DIR}"
+  mkdir -p ${UNDO_DIR}
+fi
+
 for name in *; do
   target="$HOME/.$name"
   if [ -e "$target" ]; then
